6aca0f45c8bbecea400c7e73607b13e653f67913
Karen Almeida <karenalmeida61@hotmail.com>
2021-09-29 06:28:36 -0300

    criando classe ingrediente

---
+++ Ingrediente.java
@@ -0,0 +1,17 @@
+package SistemaRestaurante;
+
+public class Ingrediente {
+    package sistema_loja;
+
+    public class Ingrediente {
+
+
+        public Ingrediente() {
+        }
+
+        @Override
+        public String toString() {
+            return "Ingredientes: ";
+        }
+    }
+}
